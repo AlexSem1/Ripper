743fae83cfaa744cf34bada038da6551ee915d54
Aleksei_Semenov <Aleksei_Semenov@epam.com>
2019-11-12 18:13:42 +0300

    BPP ..

---
+++ src/main/java/quoters/ProfilingController.java
@@ -0,0 +1,13 @@
+package quoters;
+
+public class ProfilingController implements com.epam.quoters.ProfilingControllerMBean {
+    private boolean enabled = true;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
